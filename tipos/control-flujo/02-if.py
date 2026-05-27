e44014c9d84b6af4ceab0689534eba27ca092c8d
Alexis Canto <alexiscantogodoymail.com>
2026-05-26 22:01:20 -0400

    cambio de nombre calculadora, conversion tipos, control de flujo

---
+++ tipos/control-flujo/02-if.py
@@ -0,0 +1,12 @@
+edad = 13
+if edad > 65:
+    print("Puede ver la pelicula con un super descuento")
+elif edad > 54:
+    print("Puedes ver la pelicula con un descuento")
+elif edad > 17:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes entrar")
+    print("Ve a otro lado")
+
+print("Listo")
